56a31ea9a90f028f636f66c1de3aa5462e5945ea
Dmitry Kuzmenkov <dmitry@wagh.ru>
2014-12-21 05:02:32 +0300

    Php-fpm taks config moved to plugin. Removed hosts, tasks files

--- skel/app/plugins/nginx/configure.php
+++ skel/app/plugins/nginx/configure.php
@@ -2,3 +2,5 @@
 App::configure(__DIR__, [
   '%NGINX_ROUTE_FILE%' => config('common.nginx_route_file'),
 ]);
+
+App::exec('task add "php-fpm -c $CONFIG_DIR/php.ini -y $CONFIG_DIR/php-fpm.conf -F #php-fpm"');
