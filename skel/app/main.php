be4a091bbdd41c3d355e1216fc2938304ff3c0b6
Dmitry Kuzmenkov <dmitry@wagh.ru>
2017-09-16 13:00:45 +0700

    Add extra headers with response time by default in main file

--- skel/app/main.php
+++ skel/app/main.php
@@ -1,4 +1,5 @@
 <?php
+$start_ts = microtime(true);
 include getenv('KISS_CORE');
 App::start();
 $Response = Response::create(200);
@@ -6,5 +7,11 @@ $View = App::process(Request::create(), $Response)
   ->prepend('_head')
   ->append('_foot')
 ;
-$Response->send((string) $View->render());
+$Response
+  ->header('X-Frame-Options', 'DENY')
+  ->header('X-XSS-Protection', '1; mode=block')
+  ->header('Content-Security-Policy', "frame-ancestors 'none'")
+  ->header('X-Response-Time', bcmul(microtime(true) - $start_ts, 1000, 0))
+  ->send((string) $View->render())
+;
 App::stop();
