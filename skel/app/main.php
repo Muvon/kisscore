3c2a7157b0c6072d82d238683418c52ee557fa44
Dmitry Kuzmenkov <dmitry@wagh.ru>
2018-02-07 05:27:17 +0700

    Add header to prevent sniff

--- skel/app/main.php
+++ skel/app/main.php
@@ -10,6 +10,7 @@ $View = App::process(Request::create(), $Response)
 $Response
   ->header('X-Frame-Options', 'DENY')
   ->header('X-XSS-Protection', '1; mode=block')
+  ->header('X-Content-Type-Options', 'nosniff')
   ->header('Content-Security-Policy', "frame-ancestors 'none'")
   ->header('X-Response-Time', bcmul(microtime(true) - $start_ts, 1000, 0))
   ->send((string) $View->render())
