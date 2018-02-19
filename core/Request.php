7775660bcfb5d2964d8caf5cdc24020eb1dc69c5
Dmitry Kuzmenkov <dmitry@wagh.ru>
2018-02-20 00:31:16 +0700

    Fix logic when fetching real ip under proxy

--- core/Request.php
+++ core/Request.php
@@ -108,8 +108,7 @@ class Request {
   protected static function parseRealIp() {
     self::$real_ip = self::$ip;
     if (self::$xff && self::$xff !== self::$ip) {
-      self::$real_ip = str_replace(self::$ip, '', self::$xff);
-      self::$real_ip = trim(self::$real_ip, ' ,');
+      self::$real_ip = trim(strtok(self::$ip, ','));
     }
   }
 
