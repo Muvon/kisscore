fb70b51666fd8b5ac7faca9541912f108d1685ff
Don Hardman <don.hardman@muvon.io>
2021-03-07 19:51:28 +0700

    Change interface how we use Cookie class to set it

--- core/Cookie.php
+++ core/Cookie.php
@@ -29,31 +29,27 @@ final class Cookie {
    * Set new cookie. Replace if exists
    * @param string $name
    * @param string $value
-   * @param int $time Expire at time as timestamp
-   * @param string $path Cookie save path
+   * @param array $options
    * @return void
    */
-  public static function set(string $name, string $value, int $time, string $path = '/', string $domain = null): void {
+  public static function set(string $name, string $value, array $options = []): void {
     static::$cookies[$name] = [
       'name' => $name,
       'value' => $value,
-      'time' => $time,
-      'path' => $path,
-      'domain' => $domain ?? config('common.domain')
+      'options' => $options
     ];
   }
 
   /**
    * Add new cookie. Create new only if not exists
    * @param string $name
-   * @param mixed $value
-   * @param int $time Expire at time as timestamp
-   * @param string $path Cookie save path
+   * @param string $value
+   * @param array $options
    * @return void
    */
-  public static function add(string $name, mixed $value, int $time, string $path = '/', string $domain = null): void {
+  public static function add(string $name, string $value, array $options = []): void {
     if (!filter_has_var(INPUT_COOKIE, $name)) {
-      static::set($name, $value, $time, $path, $domain);
+      static::set($name, $value, $options);
     }
   }
 
@@ -62,7 +58,14 @@ final class Cookie {
    */
   public static function send(): void {
     foreach (static::$cookies as $cookie) {
-      setcookie($cookie['name'], $cookie['value'], $cookie['time'], $cookie['path'], $cookie['domain'] ?? null, config('common.proto') === 'https', str_starts_with(getenv('SERVER_PROTOCOL'), 'HTTP'));
+      $options = array_merge($cookie['options'], [
+        'domain' => $cookie['domain'] ?? config('common.domain'),
+        'path' => $cookie['path'] ?? '/',
+        'expires' => $cookie['expires'] ?? 0,
+        'secure' => $cookie['secure'] ?? config('common.proto') === 'https',
+        'httponly' => $cookie['httponly'] ?? str_starts_with(getenv('SERVER_PROTOCOL'), 'HTTP'),
+      ]);
+      setcookie($cookie['name'], $cookie['value'], $options);
     }
   }
 }
