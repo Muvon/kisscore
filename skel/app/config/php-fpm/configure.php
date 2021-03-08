31614648b96632b6626e566d195c33deca35572c
Don Hardman <don.hardman@muvon.io>
2021-03-08 14:17:37 +0700

    Change path to save session path in tmp dir

--- skel/app/config/php-fpm/configure.php
+++ skel/app/config/php-fpm/configure.php
@@ -1,8 +1,13 @@
 <?php
+$session_dir = getenv('TMP_DIR') . '/sessions';
+if (!is_dir($session_dir)) {
+  mkdir($session_dir, 0755);
+}
+
 Env::configure(__DIR__, [
   '{{UPLOAD_MAX_FILESIZE}}' => config('common.upload_max_filesize'),
   '{{ASSERTIONS}}' => App::$debug ? 1 : -1,
   '{{SESSION_SAVE_HANDLER}}' => 'files',
   '{{SESSION_COOKIE_SECURE}}' => config('common.proto') === 'https' ? 'on' : 'off',
-  '{{SESSION_SAVE_PATH}}' => getenv('TMP_DIR')
+  '{{SESSION_SAVE_PATH}}' => $session_dir
 ]);
