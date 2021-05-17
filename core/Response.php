6e8f6ae84bf3b254289b3ae5b74dfbc5c6537024
Don Hardman <don.hardman@muvon.io>
2021-05-17 20:52:52 +0700

    Add 429 HTTP code support in Response

--- core/Response.php
+++ core/Response.php
@@ -65,6 +65,7 @@ final class Response {
     415 => 'Unsupported Media Type',
     416 => 'Requested Range Not Satisfiable',
     417 => 'Expectation Failed',
+    429 => 'Too Many Requests',
 
     500 => 'Internal Server Error',
     501 => 'Not Implemented',
