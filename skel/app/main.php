d289df7e4202fbe9d2f79ed280373f5646be0eea
Don Hardman <don.hardman@muvon.io>
2022-11-16 16:40:06 +0000

    Update skel files to reflect codestyle rules

--- skel/app/main.php
+++ skel/app/main.php
@@ -1,12 +1,12 @@
-<?php
+<?php declare(strict_types=1);
+
 include getenv('KISS_CORE');
 App::start();
 
 // Process action and get view template if have
 $View = App::process()
   ->prepend('_head')
-  ->append('_foot')
-;
+  ->append('_foot');
 
-Response::current()->send((string) $View->render());
+Response::current()->send((string)$View->render());
 App::stop();
