a328d0c325a4595f76e6938b877738dbbbbfe05a
Don Hardman <don.hardman@muvon.io>
2024-07-29 08:01:54 +0700

    Adapt random password generation to work on osx also

--- func.sh
+++ func.sh
@@ -1,11 +1,18 @@
 #!/usr/bin/env bash
+
 ssh_random_string() {
-  symbols=${1-'A-Za-z0-9'}
+	symbols=${1-'A-Za-z0-9'}
 
-  length=40
-  if [[ $2 =~ ^[0-9]+$ ]]; then
-    length=$2
-  fi
+	length=40
+	if [[ $2 =~ ^[0-9]+$ ]]; then
+		length=$2
+	fi
 
-  < /dev/urandom tr -dc ${symbols} | head -c${length} && echo
+	if [[ "$OSTYPE" == "darwin"* ]]; then
+		LC_ALL=C tr -dc "$symbols" < /dev/urandom | head -c "$length"
+	else
+		tr -dc "$symbols" < /dev/urandom | head -c "$length"
+	fi
+	echo
 }
+
